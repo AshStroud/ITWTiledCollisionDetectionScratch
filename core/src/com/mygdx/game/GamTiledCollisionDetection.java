1796a696b75d85f4a265adad1a8d3292237f6772
Ashleigh <ashcstroud@hotmail.com>
2016-04-22 00:18:41 -0400

    Got Collision Detection working, fixed gradle errors

--- core/src/com/mygdx/game/GamTiledCollisionDetection.java
+++ core/src/com/mygdx/game/GamTiledCollisionDetection.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -26,14 +27,10 @@ import java.util.ArrayList;
 //Sources: Implementing Collison Detection with Tiled: https://www.youtube.com/watch?v=MT5YwZsQnF8
 //													https://github.com/libgdx/libgdx/wiki/Tile-maps
 //			Drawing Rectangle to Screen: https://www.youtube.com/watch?v=xdc_1Pf-jnA
+//			Implementing Collision Detection using Object Layers: http://stackoverflow.com/questions/22152925/libgdx-tiled-map-collision-detection
 public class GamTiledCollisionDetection extends ApplicationAdapter {
-	//SpriteBatch batch;
-	//Texture img;
 	private static final int nCols = 4;
 	private static final int nRows = 4;
-
-	//int nHeight = 480;
-	//int nWidth = 640;
 
 	SpriteBatch sbBatch;
 	Texture txSprite;
@@ -50,18 +47,17 @@ public class GamTiledCollisionDetection extends ApplicationAdapter {
 	OrthogonalTiledMapRenderer orthotmrRenderer;
 	OrthographicCamera ocMainCam;
 
-	ArrayList<Rectangle> arlRectCollisionDetection = new ArrayList <Rectangle> ();
+	ArrayList<Rectangle> arlRectObjectBounds = new ArrayList<Rectangle>();
 	Rectangle rectSprite;
-	Rectangle rectCollision;
 	String sDirection;
 
-	ShapeRenderer srSpriteRect;
-	ShapeRenderer srCollisionRect;
+	RectangleMapObject rmoCollisionRect;
+	MapObjects moCollisionDetection;
+	Rectangle rectObjectBounds;
 	@Override
 	public void create () {
 		sbBatch = new SpriteBatch();
-		srSpriteRect = new ShapeRenderer();
-		srCollisionRect = new ShapeRenderer();
+		rectSprite = new Rectangle();
 		txSprite = new Texture(Gdx.files.internal("CinderellaSpriteSheet.png"));
 		TextureRegion[][] tmp = TextureRegion.split(txSprite, txSprite.getWidth() / nCols, txSprite.getHeight() / nRows);
 		artrFrames = new TextureRegion[nCols * nRows];
@@ -81,28 +77,13 @@ public class GamTiledCollisionDetection extends ApplicationAdapter {
 		tmGameMap= new TmxMapLoader().load("CollisionDetectionScratchMap.tmx");
 		orthotmrRenderer = new OrthogonalTiledMapRenderer(tmGameMap);
 		//Creating Bounds for Collision Detection
-		MapObjects moCollisionDetection = tmGameMap.getLayers().get("Object Layer 1").getObjects();
-		/*for(int i = 0; i < 20; i++){
-			for(int j = 0; j < 20; j++){
-				TiledMapTileLayer TmTlTrees = (TiledMapTileLayer) tmGameMap.getLayers().get("Foreground");
-				Cell cCurrentTile = new Cell();
+		moCollisionDetection = tmGameMap.getLayers().get("Object Layer 1").getObjects();
+		for (int i = 0; i < moCollisionDetection.getCount(); i++) {
+			rmoCollisionRect = (RectangleMapObject) moCollisionDetection.get(i);
+			rectObjectBounds = rmoCollisionRect.getRectangle();
 
-				if(TmTlTrees.getCell(i, j)!= null){
-					cCurrentTile = TmTlTrees.getCell(i, j);
-					System.out.println(i + " , " + j + " , " + cCurrentTile.getTile().getId());
-					//System.out.println(TmTlTrees.getCell(i, j).)
-					arlRectCollisionDetection.add(new Rectangle(i * 64, j * 64, 64, 64));
-					System.out.println("Added rectangle!");
-				}
-			}
-		}*/
-
-		for(int i = 0; i < arlRectCollisionDetection.size(); i++){
-			srCollisionRect.begin(ShapeType.Filled);
-			srCollisionRect.setColor(Color.RED);
-			srCollisionRect.rect(arlRectCollisionDetection.get(i).getX(), arlRectCollisionDetection.get(i).getY(), arlRectCollisionDetection.get(i).getWidth(), arlRectCollisionDetection.get(i).getHeight());
-			System.out.println("Drawing Rectangle");
-			srCollisionRect.end();
+			arlRectObjectBounds.add(rectObjectBounds);
+			System.out.println("Rectangle Added!");
 		}
 	}
 
@@ -149,29 +130,48 @@ public class GamTiledCollisionDetection extends ApplicationAdapter {
 		orthotmrRenderer.render();
 		ocMainCam.update();
 
-		//OrthoGraphic Camera
-		/*ocMainCam.position.set(fSpriteX, fSpriteY, 0);
-		//OcCam.setToOrtho(false, , 0);
-		sbBatch.setProjectionMatrix(ocMainCam.combined);
-		ocMainCam.update();*/
-
 		//Draw Sprites
 		sbBatch.begin();
 		sbBatch.draw(trCurrentFrame, (int) fSpriteX, (int) fSpriteY);
-		//System.out.println("Sprite drawn at" + fSpriteX + " and y value" + fSpriteY);
 		sbBatch.end();
 
-		srSpriteRect.begin(ShapeType.Filled);
-		//srSpriteRect.set(rectSprite);
-		srSpriteRect.setColor(Color.BLUE);
-		srSpriteRect.rect(fSpriteX, fSpriteY, trCurrentFrame.getRegionWidth(), trCurrentFrame.getRegionHeight());
-		srSpriteRect.end();
+		rectSprite.set(fSpriteX, fSpriteY, trCurrentFrame.getRegionWidth(), trCurrentFrame.getRegionHeight());
 
-		rectSprite = new Rectangle(fSpriteX, fSpriteY,fSpriteX +trCurrentFrame.getRegionWidth(),fSpriteY+ trCurrentFrame.getRegionHeight());
-		//rectSprite = new Rectangle(fSpriteX, fSpriteY, txSprite.getWidth(), txSprite.getHeight());
+		for (int i = 0; i < arlRectObjectBounds.size(); i++) {
+			if(rectSprite.overlaps(arlRectObjectBounds.get(i))) {
+				System.out.println("Collision Detected");
+				if(sDirection == "Up"){
+					fSpriteY -= 1f;
+					//System.out.println("Rectangle Location:" + arlRectCollisionDetection.get(i));
+					//System.out.println("Player Location:" + "X:" + fSpriteX + "Y: " + fSpriteY);
+				}
+				else if(sDirection == "Down"){
+					fSpriteY += 1f;
+					//System.out.println("Rectangle Location:" + arlRectCollisionDetection.get(i));
+					//System.out.println("Player Location:" + "X:" + fSpriteX + "Y: " + fSpriteY);
+				}
+				else if(sDirection == "Right") {
+					fSpriteX -= 1f;
+					//System.out.println("Rectangle Location:" + arlRectCollisionDetection.get(i));
+					//System.out.println("Player Location:" + "X:" + fSpriteX + "Y: " + fSpriteY);
+				}
+				else if(sDirection == "Left"){
+					fSpriteX += 1f;
+					//System.out.println("Rectangle Location:" + arlRectCollisionDetection.get(i));
+					//System.out.println("Player Location:" + "X:" + fSpriteX + "Y: " + fSpriteY);
+					//System.out.println("Play Rectangle: " + rectSprite);
+				}
 
+			}
 
-		for(int i = 0; i < arlRectCollisionDetection.size(); i++){
+		}
+
+	}
+}
+
+//Old Collision Detection Code
+
+		/*for(int i = 0; i < arlRectCollisionDetection.size(); i++){
 			if(rectSprite.overlaps(arlRectCollisionDetection.get(i))){
 				System.out.println("Collision detected!");
 				System.out.println(sDirection);
@@ -197,7 +197,19 @@ public class GamTiledCollisionDetection extends ApplicationAdapter {
 					//System.out.println("Play Rectangle: " + rectSprite);
 				}
 			}
+		}*/
+//Old Setup for Collision Detection
+/*for(int i = 0; i < 20; i++){
+			for(int j = 0; j < 20; j++){
+				TiledMapTileLayer TmTlTrees = (TiledMapTileLayer) tmGameMap.getLayers().get("Foreground");
+				Cell cCurrentTile = new Cell();
 
-		}
-	}
-}
+				if(TmTlTrees.getCell(i, j)!= null){
+					cCurrentTile = TmTlTrees.getCell(i, j);
+					System.out.println(i + " , " + j + " , " + cCurrentTile.getTile().getId());
+					//System.out.println(TmTlTrees.getCell(i, j).)
+					arlRectCollisionDetection.add(new Rectangle(i * 64, j * 64, 64, 64));
+					System.out.println("Added rectangle!");
+				}
+			}
+		}*/
